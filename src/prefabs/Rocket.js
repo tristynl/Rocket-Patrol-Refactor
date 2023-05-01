2ceb902f970d99de8da33c275f85704728d6598b
Tristyn Lai <tlai5@ucsc.edu>
2023-05-01 14:36:40 -0700

    Had to Restart

---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,41 @@
+//Rocket prefab
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        scene.add.existing(this);   //Add to existing, displayList, updateList
+        this.isFiring = false;      //Track rocket's firing status
+        this.moveSpeed = 2;         //Pixels per frame
+        this.sfxRocket = scene.sound.add('sfx_rocket')  //Add rocket sfx
+    }
+
+    update() {
+        //Left/right movement
+        if(!this.isFiring) {
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width) {
+                this.x -= this.moveSpeed;
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+                this.x += this.moveSpeed;
+            }
+        }
+        //Fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
+            this.isFiring = true;
+            this.sfxRocket.play();
+        }
+        //If fired, move up
+        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+            this.y -= this.moveSpeed;
+        }
+        //Reset on miss
+        if(this.y <= borderUISize * 3 + borderPadding) {
+            this.reset();
+        }
+    }
+
+    //Reset rocket to "ground"
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
+    }
+}
