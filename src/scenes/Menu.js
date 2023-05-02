8f5c4e4bd7b0f761161783d9dff3eecc2f2362d8
Tristyn Lai <tlai5@ucsc.edu>
2023-05-02 16:30:17 -0700

    Added Art Assets In Game

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -15,7 +15,7 @@ class Menu extends Phaser.Scene {
         let menuConfig = {
             fontFamily: 'Courier',
             fontSize: '28px',
-            backgroundColor: '#F3B141',
+            backgroundColor: '#F3B141', //
             color: '#843605',
             align: 'right',
             padding: {
@@ -28,7 +28,7 @@ class Menu extends Phaser.Scene {
         //Show menu text
         this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#00FF00';
+        menuConfig.backgroundColor = '#00FF00'; //
         menuConfig.color = '#000';
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
 
